16854b6f0cc76e0d6e0a25f7196563541e3cf260
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2025-04-09 22:00:00 +0200

    refactor(ui-localization): delete package

--- packages/ui/localization/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { useTranslation } from 'react-i18next'
-
-export * from './i18n'
-export * from './language_detector'
-export * from './resources'
--- packages/ui/localization/src/resources.ts
+++
@@ -1,7 +0,0 @@
-import commonEng from './locales/en/common.json' with { type: 'json' }
-import commonEs from './locales/es/common.json' with { type: 'json' }
-
-export const resources = {
-	es: { common: commonEs },
-	en: { common: commonEng },
-}
